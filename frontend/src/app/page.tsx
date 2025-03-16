2f28eeb86f8cbe49814531dea8772d8ff051ad20
Ivan <statkevich.ivan999@gmail.com>
2025-03-16 20:49:14 +0200

    Refactor current time calculation in Home component for accuracy. Update the visual marker's opacity to enhance visibility on the time grid.

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,12 +2,14 @@ export default function Home() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
-  const currentTimePercentage = ((hours - 5 + minutes / 60) / 24) * 100;
+  const currentTimePercentage =
+    (((hours - 2) * 60 + minutes) * 100) / (24 * 60);
 
+  console.log();
   return (
     <div className="w-full h-[calc(100vh-6rem)] bg-muted/50 rounded-lg overflow-auto flex flex-col justify-between relative">
       <div
-        className="absolute w-full h-0.5 bg-sidebar-primary z-10"
+        className="absolute w-full h-0.5 bg-sidebar-primary/50 z-10"
         style={{ top: `${currentTimePercentage}%` }}
       />
 
