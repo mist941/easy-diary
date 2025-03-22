1c25e70a2a3224a6e067814c0a755c088292b9fc
Ivan <statkevich.ivan999@gmail.com>
2025-03-22 11:58:17 +0200

    Update Calendar and DatePicker components to enhance date selection functionality. Modify Calendar's styling for today's date and ensure DatePicker passes the selected date to the Calendar component.

--- frontend/src/components/calendar.tsx
+++ frontend/src/components/calendar.tsx
@@ -50,7 +50,8 @@ function Calendar({
           'day-range-end aria-selected:bg-primary aria-selected:text-primary-foreground',
         day_selected:
           'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
-        day_today: 'bg-accent text-accent-foreground',
+        day_today:
+          'border border-accent-foreground bg-transparent text-accent-foreground font-medium',
         day_outside:
           'day-outside text-muted-foreground aria-selected:text-muted-foreground',
         day_disabled: 'text-muted-foreground opacity-50',
--- frontend/src/components/date-picker.tsx
+++ frontend/src/components/date-picker.tsx
@@ -5,7 +5,7 @@ import { SidebarGroup, SidebarGroupContent } from '@/components/sidebar';
 import useCurrentSelectedDateStore from '@/store/currentSelectedDateStore';
 
 export function DatePicker() {
-  const { setDate } = useCurrentSelectedDateStore();
+  const { setDate, date } = useCurrentSelectedDateStore();
 
   const changeDate = (date: Date) => {
     setDate(date);
@@ -16,6 +16,7 @@ export function DatePicker() {
       <SidebarGroupContent>
         <Calendar
           onDayClick={changeDate}
+          selected={date}
           className="[&_[role=gridcell].bg-accent]:bg-sidebar-primary [&_[role=gridcell].bg-accent]:text-sidebar-primary-foreground [&_[role=gridcell]]:w-[33px]"
         />
       </SidebarGroupContent>
