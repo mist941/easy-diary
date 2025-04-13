7dc628b226442238f70c5c62102e2b6cd186f01c
Ivan <statkevich.ivan999@gmail.com>
2025-04-13 21:38:52 +0300

    Refactor AppSidebar component imports for better organization
    - Updated import statements in AppSidebar.tsx to streamline component imports from Sidebar.
    - Added missing exports for SidebarSeparator and SidebarContent in Sidebar index file.

--- frontend/src/components/layouts/AppSidebar/AppSidebar.tsx
+++ frontend/src/components/layouts/AppSidebar/AppSidebar.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { DatePicker } from '@/components/layouts/DatePicker';
-
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarHeader,
-  SidebarSeparator,
-} from '@/components/layouts/Sidebar/Sidebar';
+import { Sidebar } from '@/components/layouts/Sidebar';
+import { SidebarHeader } from '@/components/layouts/Sidebar';
+import { SidebarContent } from '@/components/layouts/Sidebar';
+import { SidebarFooter } from '@/components/layouts/Sidebar';
+import { SidebarSeparator } from '@/components/layouts/Sidebar';
 
 function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
--- frontend/src/components/layouts/Sidebar/index.tsx
+++ frontend/src/components/layouts/Sidebar/index.tsx
@@ -1,3 +1,5 @@
 export { Sidebar } from './Sidebar';
 export { SidebarHeader } from './SidebarHeader';
 export { SidebarFooter } from './SidebarFooter';
+export { SidebarSeparator } from './SidebarSeparator';
+export { SidebarContent } from './SidebarContent';
