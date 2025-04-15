89101190656ae20b9972792d38b4620165e54333
Ivan <statkevich.ivan999@gmail.com>
2025-04-15 11:52:24 +0300

    Refactor configuration settings and enhance logging for improved clarity
    - Updated the Settings class in config.py to use Field for environment variable aliases, improving clarity and consistency.
    - Refactored model_config for better organization of environment file settings.
    - Added additional log entries in access.log for enhanced tracking of API requests and responses.

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,25 +1,23 @@
 from pathlib import Path
 from typing import Optional
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
 
 class Settings(BaseSettings):
     DATABASE_URL: Optional[str] = None
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    postgres_user: str
-    postgres_password: str
-    postgres_db: str
+    postgres_user: str = Field(alias="POSTGRES_USER")
+    postgres_password: str = Field(alias="POSTGRES_PASSWORD")
+    postgres_db: str = Field(alias="POSTGRES_DB")
+    db_host: str = Field(alias="DB_HOST")
 
-    class Config:
-        env_file = str(Path(__file__).parent.parent.parent.parent / ".env")
-        env_file_encoding = "utf-8"
-        fields = {
-            "postgres_user": {"env": "POSTGRES_USER"},
-            "postgres_password": {"env": "POSTGRES_PASSWORD"},
-            "postgres_db": {"env": "POSTGRES_DB"},
-        }
+    model_config = SettingsConfigDict(
+        env_file=str(Path(__file__).parent.parent.parent.parent / ".env"),
+        env_file_encoding="utf-8",
+    )
 
 
 settings = Settings()
