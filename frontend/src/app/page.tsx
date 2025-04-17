62b36b7e88aa26f73526e3936e10f6d2baecfc6f
Ivan <statkevich.ivan999@gmail.com>
2025-04-17 12:06:29 +0300

    Remove unused notes fetching logic from Home component for code cleanup

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { Diary } from '@/components/features/diary/components';
-import notesService from '@/components/features/notes/api';
 
 export default async function Home() {
-  const notes = await notesService.getNotes('2025-04-17');
-  console.log(notes);
   return <Diary />;
 }
