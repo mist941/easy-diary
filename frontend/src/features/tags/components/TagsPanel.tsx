fd287b33be63f81d09af828effed9d08cd68039a
Ivan <statkevich.ivan999@gmail.com>
2025-04-26 20:40:16 +0300

    Refactor TagsPanel layout to improve search input styling and alignment with the Add Tag button.

--- frontend/src/features/tags/components/TagsPanel.tsx
+++ frontend/src/features/tags/components/TagsPanel.tsx
@@ -4,15 +4,12 @@ import { TagsTable } from './TagsTable';
 
 function TagsPanel() {
   return (
-    <div>
-      <div className="flex flex-col gap-4">
-        <h1 className="text-2xl font-bold">Tags</h1>
-        <div className="flex flex-row gap-4">
-          <Input placeholder="Search" />
-          <Button>Add Tag</Button>
-        </div>
-        <TagsTable />
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row gap-4 justify-between">
+        <Input className="w-1/3" placeholder="Search" />
+        <Button>Add Tag</Button>
       </div>
+      <TagsTable />
     </div>
   );
 }
