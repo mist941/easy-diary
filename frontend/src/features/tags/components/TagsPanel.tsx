4c6d7ba92a4adcb34ba1c3e7f5c55d9769768095
Ivan <statkevich.ivan999@gmail.com>
2025-10-06 10:31:59 +0300

    some changes for dialog component

--- frontend/src/features/tags/components/TagsPanel.tsx
+++ frontend/src/features/tags/components/TagsPanel.tsx
@@ -7,17 +7,19 @@ import { Dialog, DialogTrigger } from '@/components/ui/Dialog';
 import { TagForm } from './TagForm';
 import { ITagRequest } from '../types';
 import { useTagsManagement } from '../hooks/useTagsManagement';
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
 function TagsPanel() {
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const { tags, createTag } = useTagsManagement();
 
   const handleSubmit = useCallback((values: ITagRequest) => {
     createTag(values);
+    setIsDialogOpen(false);
   }, []);
 
   return (
-    <Dialog>
+    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
       <div className="flex flex-col gap-4">
         <div className="flex flex-row gap-4 justify-between">
           <Input className="w-1/3" placeholder="Search" />
@@ -27,7 +29,7 @@ function TagsPanel() {
         </div>
         <TagsTable tags={tags} />
       </div>
-      <TagForm onSubmit={handleSubmit} />
+      {isDialogOpen && <TagForm onSubmit={handleSubmit} />}
     </Dialog>
   );
 }
